b28420124a14622fd131cc0940e011e5f0256692
truongvinguyen <nguyentruongvi2203@gmail.com>
2022-07-22 01:40:22 +0700

    momo-payment

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -18,6 +18,24 @@ class CheckoutController extends Controller
     public function checkout(){
         return view('client.checkout.checkout');
     }
+    public function execPostRequest($url, $data)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($data))
+        );
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        //execute post
+        $result = curl_exec($ch);
+        //close connection
+        curl_close($ch);
+        return $result;
+    }
     public function order(Request $request){
         $validate= $request->validate([
             'customer_name'=>['required','min:5'],
@@ -39,11 +57,18 @@ class CheckoutController extends Controller
         //    'customer_provinces.required'=>'Tỉnh / thành phố không được bỏ trống',
         //    'customer_district.required'=>'Quận / huyện không được bỏ trống',         
         ]);
+        //ship
         if(Session()->get('cart')->totalPrice > 500000){
             $ship_price=0;
         }
         else{
             $ship_price=30000;
+        }
+        //image  notification
+        if(Session()->has('userAddress')){
+            $notification_image=Session()->get('userImage');
+            }
+            else{ $notification_image="1";
         }
         if($request->payment_methods==1){
            $data['customer_name'] = $request->customer_name;
@@ -75,47 +100,144 @@ class CheckoutController extends Controller
                 'updated_at'=>now(),
             ]);
            }
-        }
-        else{
-            echo "thanh toán online chưa hỗ trợ";
-        }
-        if(Session()->has('userAddress')){
-            $notification_image=Session()->get('userImage');
-            }
-            else{ $notification_image="1";
-        }
-        if($request->customer_id<1){
-            $order=DB::table('order')
-            ->select('*')
-            ->where('id',$order_id)
-            ->first();
-            $order_detail=Session()->get('cart')->products;
-            Mail::send('client.checkout.check_order',compact('order','order_detail'),function($mail) use ($request){
-                $mail->to($request->customer_email);
-                $mail->subject('Xác nhận đơn hàng.');
-            });
-        }else {
-            $order=DB::table('order')
-            ->select('*')
-            ->where('id',$order_id)
-            ->first();
-            $order_detail=Session()->get('cart')->products;
-            Mail::send('client.checkout.check_order_2',compact('order','order_detail'),function($mail) use ($request){
-                $mail->to($request->customer_email);
-                $mail->subject('Đặt hàng thành công.');
-            });
-        }
-        notification::create([
+           notification::create([
             'notification_name'=>"Đơn hàng mới",
             'notification_content'=>$request->customer_name." vừa đặt 1 đơn hàng mới, bạn vui lòng xử lý đơn hàng này nhé",
             'notification_image'=>$notification_image,
             'notification_status'=>1,
             'order_id'=> $order_id
         ]);
+        if($request->customer_id<1){
+            $order_table=DB::table('order')
+            ->select('*')
+            ->where('id',$order_id)
+            ->first();
+            $order_detail=Session()->get('cart')->products;
+            Mail::send('client.checkout.check_order',compact('order_table','order_detail'),function($mail) use ($request){
+                $mail->to($request->customer_email);
+                $mail->subject('Xác nhận đơn hàng.');
+            });
+        }else {
+            $order_table=DB::table('order')
+            ->select('*')
+            ->where('id',$order_id)
+            ->first();
+            $order_detail=Session()->get('cart')->products;
+            Mail::send('client.checkout.check_order_2',compact('order_table','order_detail'),function($mail) use ($request){
+                $mail->to($request->customer_email);
+                $mail->subject('Đặt hàng thành công.');
+            });
+        }
+        Session()->forget('cart'); 
+        }
+        else{
+        //thanh toán momo
 
-        Session()->forget('cart');    
-            
-            echo "thanh toán thành công vui lòng kiểm tra mail của bạn";
+        $endpoint = "https://test-payment.momo.vn/v2/gateway/api/create";
+
+
+        $partnerCode = 'MOMOBKUN20180529';
+        $accessKey = 'klm05TvNBzhg7h7j';
+        $secretKey = 'at67qH6mk8w5Y1nAyMoYKMWACiEi2bsa';
+        $orderInfo = "Thanh toán qua MoMo";
+        $amount = Session()->get('cart')->totalPrice+$ship_price;
+        $orderId = time() . "";
+        $redirectUrl = "http://127.0.0.1:8000/checkout";
+        $ipnUrl = "http://127.0.0.1:8000/checkout";
+        $extraData = "";
+        
+
+
+            $requestId = time() . "";
+            $requestType = "payWithATM";
+            // $extraData = ($_POST["extraData"] ? $_POST["extraData"] : "");
+            //before sign HMAC SHA256 signature
+            $rawHash = "accessKey=" . $accessKey . "&amount=" . $amount . "&extraData=" . $extraData . "&ipnUrl=" . $ipnUrl . "&orderId=" . $orderId . "&orderInfo=" . $orderInfo . "&partnerCode=" . $partnerCode . "&redirectUrl=" . $redirectUrl . "&requestId=" . $requestId . "&requestType=" . $requestType;
+            $signature = hash_hmac("sha256", $rawHash, $secretKey);
+            $data = array('partnerCode' => $partnerCode,
+                'partnerName' => "Test",
+                "storeId" => "MomoTestStore",
+                'requestId' => $requestId,
+                'amount' => $amount,
+                'orderId' => $orderId,
+                'orderInfo' => $orderInfo,
+                'redirectUrl' => $redirectUrl,
+                'ipnUrl' => $ipnUrl,
+                'lang' => 'vi',
+                'extraData' => $extraData,
+                'requestType' => $requestType,
+                'signature' => $signature);
+            $result = $this->execPostRequest($endpoint, json_encode($data));
+            $jsonResult = json_decode($result, true);  // decode json
+            //thanh toán
+            $order['customer_name'] = $request->customer_name;
+            $order['customer_phone'] = $request->customer_phone;
+            $order['customer_email'] = $request->customer_email;
+            $order['payment_methods'] = $request->payment_methods;
+            if(Session()->has('userAddress')){
+            $order['customer_address'] = $request->customer_address;
+            }else{
+            $order['customer_address'] = $request->customer_provinces." / ".$request->customer_district." / ".$request->customer_address;
+            }
+            $order['customer_note'] = $request->customer_note;
+            $order['ship_fee']= $ship_price;
+            $order['total_quantity']=Session()->get('cart')->totalQty;
+            $order['total_price']=Session()->get('cart')->totalPrice+$ship_price;
+            $order['status']=$request->status;
+            $order['customer_id']=$request->customer_id;
+            $order['created_at']=now();
+            $order['updated_at']=now();
+ 
+            $order_id =DB::table('order')->insertGetId($order);
+ 
+            foreach(Session()->get('cart')->products as $item){
+             order_detail::create([
+                 'order_id'=>  $order_id,
+                 'product_id'=>$item['productInfo']->id,
+                 'quantity'=>$item['quanty'],
+                 'created_at'=>now(),
+                 'updated_at'=>now(),
+             ]);
+            }
+            notification::create([
+             'notification_name'=>"Đơn hàng mới",
+             'notification_content'=>$request->customer_name." vừa đặt 1 đơn hàng mới, bạn vui lòng xử lý đơn hàng này nhé",
+             'notification_image'=>$notification_image,
+             'notification_status'=>1,
+             'order_id'=> $order_id
+         ]);
+
+         if($request->customer_id<1){
+            $order_table=DB::table('order')
+            ->select('*')
+            ->where('id',$order_id)
+            ->first();
+            $order_detail=Session()->get('cart')->products;
+            Mail::send('client.checkout.check_order',compact('order_table','order_detail'),function($mail) use ($request){
+                $mail->to($request->customer_email);
+                $mail->subject('Xác nhận đơn hàng.');
+            });
+        }else {
+            $order_table=DB::table('order')
+            ->select('*')
+            ->where('id',$order_id)
+            ->first();
+            $order_detail=Session()->get('cart')->products;
+            Mail::send('client.checkout.check_order_2',compact('order_table','order_detail'),function($mail) use ($request){
+                $mail->to($request->customer_email);
+                $mail->subject('Đặt hàng thành công.');
+            });
+        }
+         Session()->forget('cart'); 
+        
+            //Just a example, please check more in there
+           return redirect()->to($jsonResult['payUrl']);
+            // header('Location: ' . $jsonResult['payUrl']);
+    
+
+        //
+        }
+    echo "thanh toán thành công vui lòng kiểm tra mail của bạn";
     }
     public function accept($id){
        
