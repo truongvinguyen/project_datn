a1951dda9268a79508d5fc7930180dde361a9b1a
Btoann <nguyenbaotoan2001@gmail.com>
2022-08-04 13:31:45 +0700

    change url config

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     */
 
     // 'url' => env('APP_URL', 'https://trendyshop.weit.tech'),
-    'url' => env('APP_URL'),
+    'url' => 'https://trendyshop.weit.tech',
 
     'asset_url' => env('ASSET_URL'),
 
