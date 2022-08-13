34f49de4dedc67b876c2f262162fe760c02f8178
nhitranyen <nhitty28@gmail.com>
2022-08-13 23:42:59 +0700

    update about-us

--- resources/views/client/others/aboutUs.blade.php
+++ resources/views/client/others/aboutUs.blade.php
@@ -6,31 +6,9 @@ Trang Về Chúng Tôi
 <div class="main container">
  <div class="row">
      <div class="about-page">
-        <div class="col-xs-12 col-sm-6"> 
-          
-          <h1>Chào mừng bạn đến với <span class="text_color">Trendy fashion</span></h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras lacus metus, convallis ut leo nec, tincidunt eleifend justo. Ut felis orci, hendrerit a pulvinar et, gravida ac lorem. Sed vitae molestie sapien, at sollicitudin tortor.<br>
-            <br>
-            Duis id volutpat libero, id vestibulum purus.Donec euismod accumsan felis, <a href="#">egestas lobortis velit tempor</a> vitae. Integer eget velit fermentum, dignissim odio non, bibendum velit.</p>
-          <ul>
-            <li><i class="fa fa-arrow-right"></i>&nbsp; <a href="#">Suspendisse potenti. Morbi mollis tellus ac sapien.</a></li>
-            <li><i class="fa fa-arrow-right"></i>&nbsp; <a href="#">Cras id dui. Nam ipsum risus, rutrum vitae, vestibulum eu.</a></li>
-            <li><i class="fa fa-arrow-right"></i>&nbsp; <a href="#">Phasellus accumsan cursus velit. Pellentesque egestas.</a></li>
-            <li><i class="fa fa-arrow-right"></i>&nbsp; <a href="#">Lorem Ipsum generators on the Internet tend to repeat predefined.</a></li>
-          </ul>
-        </div>
-        <div class="col-xs-12 col-sm-6">
-          <div class="single-img-add sidebar-add-slider">
-            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel"> 
-              
-              <!-- Wrapper for slides -->
-              <!-- <div class="carousel-inner" role="listbox">
-                <div class="item active"> <img src="client/images/3.png" alt="slide1"> </div>
-                <div class="item"> <img src="client/images/jisoo.jpg" alt="slide2"> </div>
-                <div class="item"> <img src="client/images/rose.jpg" alt="slide3"> </div>
-              </div> -->
-            </div>
-          </div>
+        <div class="col-xs-12 col-sm-12" style="margin-bottom: 50px;"> 
+          <h2 class="about-content__title" style="margin-bottom: 20px;">Về Chúng Tôi</h2>
+          <p class="p-aboutUs"><span style="font-size:50px;">Trendy</span> được thành lập vào năm 2021, với tiền thân là đội ngũ lập trình viên chuyên nghiệp và nhiệt huyết. Khai thác sức mạnh của công nghệ, áp dụng tư duy sáng tạo và ứng dụng thực tiễn, chúng tôi cung cấp giải pháp sáng tạo và liên tục được hoàn thiện để giải quyết các thách thức mà doanh nghiệp đang gặp phải.</p>
         </div>
       </div></div>
 
@@ -40,12 +18,50 @@ Trang Về Chúng Tôi
    
       
   
-  
+  <div class="our-team" style="background: white;">
+    <div class="container">
+        <div class="slider-items-products">
+          <h1 class="about-content__title">Chúng tôi hoạt động thế nào</h1>
+         
+          <div id="our-clients-slider" class="product-flexslider hidden-buttons" style="display: flex;">
+            <img style="border-radius: 20px;height: 350px;" src="https://images.squarespace-cdn.com/content/v1/5bc840789d41495b62ce3b19/1625474039900-COXIHXD5V210SL3KR38J/logic_cover+2.png?format=750w" alt="">
+            <div style="margin: 40px auto;">
+              <h4 style="color: #CF3341;text-decoration: 2px underline;" class="about-content__title">Tầm nhìn</h4>
+              <p class="p-aboutUs"><span style="font-size:50px;">Trendy</span> phấn đấu trở thành một trong những công ty cung cấp dịch vụ hàng đầu dành cho các doanh nghiệp, uy tín, có thương hiệu. Mục tiêu của chúng tôi là trở thành thương đáng tin cậy về Website vào năm 2022. Chúng tôi luôn sẵn sàng là nơi trao gởi niềm tin lý tưởng của khách hàng.</p>
+         
+            </div>
+           </div>
+
+          <div id="our-clients-slider" class="product-flexslider hidden-buttons" style="display: flex;margin-top: 100px;">
+            <div style="margin: 40px auto;">
+              <h4 style="color: #CF3341;text-decoration: 2px underline;" class="about-content__title">Sứ mệnh</h4>
+              <p class="p-aboutUs"><span style="font-size:50px;">Trendy</span> mang trong mình sứ mệnh cung cấp những giải pháp hiệu quả, xuất phát từ tư duy sáng tạo cùng với việc đặt chữ “TÍN” lên hàng đầu. Chúng tôi chính là sự lựa chọn tốt nhất cho bạn vì: SÁNG TẠO - CHẤT LƯỢNG - UY TÍN</p>
+         
+            </div>
+            <img style="border-radius: 20px;height: 350px;" src="https://images.squarespace-cdn.com/content/v1/5bc840789d41495b62ce3b19/1625473923924-R4PTKH4V00R648MVTO2G/logic_cover+1.png?format=750w" alt="">
+            
+          </div>
+
+          <div id="our-clients-slider" class="product-flexslider hidden-buttons" style="display: flex;margin-top: 100px;">
+            <img style="border-radius: 20px;height: 350px;" src="https://images.squarespace-cdn.com/content/v1/5bc840789d41495b62ce3b19/1625474095860-MQX4SBZ4NJD2PBYUJT6F/logic_cover+3.png?format=750w" alt="">
+            
+            <div style="margin: 40px auto;">
+              <h4 style="color: #CF3341;text-decoration: 2px underline;" class="about-content__title">Văn hóa</h4>
+              <p class="p-aboutUs"><span style="font-size:50px;">Trendy</span> luôn chia sẻ, giúp đỡ lẫn nhau, luôn là anh em, đồng chí trên mọi mặt trận kinh tế - xã hội. Đề cao tinh thần sáng tạo và đề xuất của từng cá nhân từ đó tạo ra một tập thể luôn đổi mới và thích ứng với thời đại ĐỀ XUẤT - CHIA SẺ - SÁNG TẠO</p>
+         
+            </div>
+           </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
    <!-- our clients Slider -->
   <div class="our-team">
     <div class="container">
         <div class="slider-items-products">
+          <h1 class="about-content__title">Đội của chúng tôi</h1>
+          <p  class="p-aboutUs">Đội ngũ sáng lập của chúng tôi đã thực hiện thành công các dự án đa dạng khác nhau. Chính những dự án đó đã mang lại cho chúng tôi những kinh nghiệm và những kiến thức quý báu để phát triển những dịch vụ chất lượng nhất.</p>
           <div id="our-clients-slider" class="product-flexslider hidden-buttons">
             <div class="slider-items slider-width-col6"> 
               
@@ -53,8 +69,8 @@ Trang Về Chúng Tôi
               <div class="item">
                 <div class="team">
                   <div class="inner">
-                    <div class="avatar"><img src="client/images/Vi.jpg" alt="" class="img-responsive" /></div>
-                    <h5>NGuyễn Trường Vi(Leader)</h5>
+                    <div class="avatar"><img style="height: 242px;margin: 0 auto;" src="client/images/Vi.jpg" alt="" class="img-responsive" /></div>
+                    <h5>Nguyễn Trường Vi(Leader)</h5>
                     <p class="subtitle">Back End</p>
                   </div>
                 </div>
@@ -65,9 +81,21 @@ Trang Về Chúng Tôi
               <div class="item">
                 <div class="team">
                   <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
+                    <div class="avatar"><img src="client/images/Phuong.jpg" alt="" class="img-responsive" /></div>
+                    <h5>Đinh Tiến Phương</h5>
+                    <p class="subtitle">Back End</p>
+                  </div>
+                </div>
+              </div>
+              <!-- End Item --> 
+
+               <!-- Item -->
+               <div class="item">
+                <div class="team">
+                  <div class="inner">
+                    <div class="avatar"><img src="client/images/Vu.jpg" alt="" class="img-responsive" /></div>
+                    <h5>Nguyễn Lưu Phi Vũ</h5>
+                    <p class="subtitle">Back End</p>
                   </div>
                 </div>
               </div>
@@ -77,59 +105,38 @@ Trang Về Chúng Tôi
               <div class="item">
                 <div class="team">
                   <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
+                    <div class="avatar"><img src="client/images/Nhi.jpg" alt="" style="height: 242px;margin: 0 auto;" class="img-responsive" /></div>
+                    <h5>Trần Thị Yến Nhi</h5>
+                    <p class="subtitle">Back End</p>
                   </div>
                 </div>
               </div>
               <!-- End Item --> 
-              
+
               <!-- Item -->
-              <div class="item">
+            <!-- Item -->
+            <div class="item">
                 <div class="team">
                   <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
+                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" style="height: 242px;margin: 0 auto;" class="img-responsive" /></div>
+                    <h5>Nguyễn Bảo Toàn</h5>
+                    <p class="subtitle">Back End</p>
                   </div>
                 </div>
               </div>
               <!-- End Item --> 
-              <!-- Item -->
-              <div class="item">
+               <!-- Item -->
+               <div class="item">
                 <div class="team">
                   <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
+                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" style="height: 242px;margin: 0 auto;" class="img-responsive" /></div>
+                    <h5>Nguyễn Phan Trường</h5>
+                    <p class="subtitle">Back End</p>
                   </div>
                 </div>
               </div>
               <!-- End Item --> 
-              <!-- Item -->
-              <div class="item">
-                <div class="team">
-                  <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
-                  </div>
-                </div>
-              </div>
-              <!-- End Item --> 
-              <!-- Item -->
-              <div class="item">
-                <div class="team">
-                  <div class="inner">
-                    <div class="avatar"><img src="client/images/team-img04.jpg" alt="" class="img-responsive" /></div>
-                    <h5>Tom Joana</h5>
-                    <p class="subtitle">Digital Creative Director</p>
-                  </div>
-                </div>
-              </div>
-              <!-- End Item --> 
-              
+
             </div>
           </div>
         </div>
